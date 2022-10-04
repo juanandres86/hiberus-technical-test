9047c96cb200a874cec525df28aab4c4bf9a0b17
Juan Andres Diaz <juanandres86@gmail.com>
2022-10-04 12:07:43 +0200

    feat: Protected Routes

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate,
+} from 'react-router-dom'
 import LoginPage from '../pages/login/login.page'
 import SignUpPage from '../pages/signup/signup.page'
 import UsersPage from '../pages/users/users.page'
@@ -10,6 +15,19 @@ import NavBarComponent from '../components/navbar/navbar.component'
 import UpdateUserPage from '../pages/updateUser/updateUser.page'
 import CreateUserPage from '../pages/createUser/createUser.page'
 
+const ProtectedRoute = ({
+    isLogged,
+    children,
+}: {
+    isLogged: boolean
+    children: JSX.Element
+}) => {
+    if (!isLogged) {
+        return <Navigate to="/" />
+    }
+    return children
+}
+
 const MainNavigator = () => {
     const isLogged = useSelector(getIsLogged)
     return (
@@ -18,9 +36,31 @@ const MainNavigator = () => {
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/users" element={<UsersPage />} />
-                <Route path="/update" element={<UpdateUserPage />} />
-                <Route path="/create" element={<CreateUserPage />} />
+                <Route
+                    path="/users"
+                    element={
+                        <ProtectedRoute isLogged={isLogged}>
+                            <UsersPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="/update"
+                    element={
+                        <ProtectedRoute isLogged={isLogged}>
+                            <UpdateUserPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path={'/create'}
+                    element={
+                        <ProtectedRoute isLogged={isLogged}>
+                            <CreateUserPage />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="*" element={<Navigate to="/users" />} />
             </Routes>
         </Router>
     )
